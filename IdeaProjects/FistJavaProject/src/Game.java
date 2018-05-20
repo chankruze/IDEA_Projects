30cf90c9e5646a5b67f195a30806fc065c8a6dcd
geekofia <chandu.chandaniit@gmail.com>
2018-05-20 07:58:54 +0530

    backedup

---
+++ IdeaProjects/FistJavaProject/src/Game.java
@@ -0,0 +1,12 @@
+public class Game {
+    int score;
+    Game(){
+        score=0;
+        System.out.println(score);
+    }
+    Game(int startingScore){
+        score=startingScore;
+        System.out.println(score);
+    }
+
+}
---
+++ IdeaProjects/FistJavaProject/src/Main.java
@@ -0,0 +1,8 @@
+
+
+public class Main {
+    public static void main(String [] args){
+        Game test = new Game(102);
+        printf("hi");
+    }
+}
